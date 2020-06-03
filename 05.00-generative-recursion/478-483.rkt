b5d17b74af6e78519925e88bcc8f1eff71e75172
Jakub <nawias@gmail.com>
2020-06-04 01:05:14 +0200

    add exercise 481, 482, 483

--- 05.00-generative-recursion/478-483.rkt
+++ 05.00-generative-recursion/478-483.rkt
@@ -2,6 +2,8 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 478-483) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
+(require 2htdp/abstraction)
+(require racket/list)
 
 ; ==================== Exercise 478 ====================
 
@@ -97,17 +99,211 @@
   (local ((define (draw-row y-pos)
             (foldr (lambda (index-x image)
                      (if (ormap (lambda (q) (and (= (posn-x q) index-x)
-                                             (= (posn-y q) y-pos)))
-                            loq)
+                                                 (= (posn-y q) y-pos)))
+                                loq)
                          (beside (overlay QUEEN-FIG SPACE) image)
                          (beside SPACE image)))
                    empty-image
                    (range 0 n 1)))
           (define draw-grid
             (foldl (lambda (index-y image)
-                   (above (draw-row index-y) image))
+                     (above (draw-row index-y) image))
                    empty-image
                    (range 0 n 1))))
     draw-grid))
+
+; =================== End of exercise ==================
+
+; N -> [Maybe [List-of QP]]
+; finds a solution to the n queens problem 
+ 
+; data example: [List-of QP]
+(define 4QUEEN-SOLUTION-2
+  (list  (make-posn 0 2) (make-posn 1 0)
+         (make-posn 2 3) (make-posn 3 1)))
+ 
+;(define (n-queens n) #false)
+; (define (n-queens n) 4QUEEN-SOLUTION-2)
+
+; ==================== Exercise 481 ====================
+
+; N -> [[List-of QP] -> Boolean]
+(define (n-queens-solution? n)
+  (lambda (q)
+    (cond [(empty? q) #false]
+          [(<= n 3) #false]
+          [(not (= (length q) n)) #false]
+          [else
+           (local ((define all-pairs-are-not-threatning
+                     (andmap (lambda (x)
+                               (andmap (lambda (y) (not (threatening? x y)))
+                                       (remove x q)))
+                             q)))
+             all-pairs-are-not-threatning)])))
+
+;(check-expect ((n-queens-solution? 3) '()))
+#;(check-expect ((n-queens-solution? 4) '())
+                #false)
+(check-expect ((n-queens-solution? 4) 4QUEEN-SOLUTION-2)
+              #true)
+(check-expect ((n-queens-solution? 4) (list (make-posn 0 3) (make-posn 2 1) (make-posn 0 1) (make-posn 0 2)))
+              #false)
+
+; (check-satisfied (n-queens 4) (n-queens-solution? 4))
+
+; [List-of QP] -> Boolean
+; is the result equal [as a set] to one of two lists
+#;(define (is-queens-result? x)
+    (or (set=? 4QUEEN-SOLUTION-1 x)
+        (set=? 4QUEEN-SOLUTION-2 x)))
+
+; List List -> Boolean
+; determine whether input lists contain
+; the same items—regardless of order.
+; (define (set=? s1 s2) #false) ;stub
+
+(check-expect (set=? '(1 2 3) '(3 2 1)) #true)
+(check-expect (set=? '(4 2 3) '(3 2 1)) #false)
+(check-expect (set=? '() '(1)) #false)
+
+(define (set=? s1 s2)
+  (local ((define set-lengths-not-equal?
+            (not (= (length s1) (length s2)))))
+    (cond [set-lengths-not-equal? #false]
+          [else
+           (andmap (lambda (el)
+                     (member? el s2))
+                   s1)])))
+
+; =================== End of exercise ==================
+
+; ==================== Exercise 482 ====================
+
+; N -> Board 
+; creates the initial n by n board
+#;(define (board0 n) ...)
+ 
+; Board QP -> Board 
+; places a queen at qp on a-board
+#;(define (add-queen a-board qp)
+    a-board)
+ 
+; Board -> [List-of QP]
+; finds spots where it is still safe to place a queen
+#;(define (find-open-spots a-board)
+    '())
+
+; Board N -> [Maybe [List-of QP]]
+; places n queens on board; otherwise, returns #false
+#;(define (place-queens a-board n)
+    #false)
+
+#;(define (place-queens a-board n)
+    (local ((define new-board-state
+              (add-queen a-board (first (find-open-spots a-board)))))
+      (cond [(= n 0) '()]
+            [else
+             (cons new-board-state
+                   (place-queens new-board-state (- n 1)))])))
+
+; =================== End of exercise ==================
+
+; ==================== Exercise 483 ====================
+
+; A Board is a [List-of Square]
+
+(define-struct square0 [x y threatened])
+; A Square is a structure:
+;   (make-square0 N N Boolean)
+
+; Number ->  Board
+(define (board0 n)
+  (local ((define grid   (map (lambda (x)
+                                (map (lambda (y)
+                                       (make-square0 x y #f))
+                                     (range 0 n 1)))
+                              (range 0 n 1))))
+    (flatten grid)))
+
+(check-expect (board0 2) `(,(make-square0 0 0 #f)
+                           ,(make-square0 0 1 #f)
+                           ,(make-square0 1 0 #f)
+                           ,(make-square0 1 1 #f)))
+
+; Board QP -> Board 
+; places a queen at qp on a-board
+(define (add-queen a-board qp)
+  ;; todo - I could use filter on a-board to just check
+  ;; filelds that are not already thretened
+  (map (lambda (sq)
+         (local ((define sq-x (square0-x sq))
+                 (define sq-y (square0-y sq))
+                 (define sq-threatened? (threatening? qp
+                                                      (make-posn sq-x sq-y))))
+           (if sq-threatened?
+               (make-square0 sq-x sq-y #t)
+               sq)))
+       a-board))
+
+(check-expect (add-queen (board0 3) (make-posn 0 0))
+              `(,(make-square0 0 0 #t)
+                ,(make-square0 0 1 #t)
+                ,(make-square0 0 2 #t)
+                ,(make-square0 1 0 #t)
+                ,(make-square0 1 1 #t)
+                ,(make-square0 1 2 #f)
+                ,(make-square0 2 0 #t)
+                ,(make-square0 2 1 #f)
+                ,(make-square0 2 2 #t)))
+
+; Board -> [List-of QP]
+; finds spots where it is still safe to place a queen
+(define (find-open-spots a-board)
+  (foldr (lambda (sq acc)
+           (local ((define sq-x (square0-x sq))
+                   (define sq-y (square0-y sq))
+                   (define sq-threatened? (square0-threatened sq)))
+             (if sq-threatened?
+                 acc
+                 (cons (make-posn sq-x sq-y)
+                       acc))))
+         '()
+         a-board))
+                  
+
+(check-expect (find-open-spots `(,(make-square0 0 0 #t)
+                                 ,(make-square0 0 1 #f)
+                                 ,(make-square0 1 0 #t)
+                                 ,(make-square0 1 1 #f)))
+              `(,(make-posn 0 1) ,(make-posn 1 1)))
+
+(check-expect (find-open-spots `(,(make-square0 0 0 #t)
+                                 ,(make-square0 0 1 #t)))
+              '())
+
+; Board N -> [Maybe [List-of QP]]
+; places n queens on board; otherwise, returns #false
+(define (place-queens a-board n)
+  (local ((define open-spots (find-open-spots a-board)))
+    (cond [(empty? open-spots) #false]
+          [(= n 1) (list (first open-spots))]
+          [else
+           (place-queens/list open-spots a-board n)])))
+
+; [List-of Square] Board N -> [Maybe [List-of QP]
+(define (place-queens/list loq a-board n)
+    (cond [(empty? loq) #false]
+          [else
+           (local ((define candidate (place-queens (add-queen a-board (first loq)) (- n 1))))
+             (cond [(boolean? candidate)
+                    (place-queens/list (rest loq) a-board n)]
+                   [else (cons (first loq) candidate)]))]))
+
+(define (n-queens n)
+  (place-queens (board0 n) n))
+
+(check-expect (n-queens 3) #false)
+(check-satisfied (n-queens 4) (n-queens-solution? 4))
+; (render-queens 8 (n-queens 8) QUEEN-FIG)
 
 ; =================== End of exercise ==================
